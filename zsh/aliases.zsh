882f8710dab97303a4f1ccb056c80db1e569ad43
Jonathan Clem <jonathan@ticketevolution.com>
2012-11-01 19:12:01 -0400

    New alias and keybindings
    - `ls` is now aliased to `ls -Ghl`
    - vi-like keybindings in zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
 alias ..="cd .."
 alias ...="cd ../.."
 alias reload!='source $HOME/.zshrc'
-alias ls="ls -G"
+alias ls="ls -Ghl"
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,2 +1,4 @@
 fpath=($ZSH/zsh/functions $fpath)
 autoload -U $ZSH/zsh/functions/*(:t)
+
+bindkey -v # vi keybindings (<esc> to enter normal mode)
