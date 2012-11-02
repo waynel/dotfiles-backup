49ab6442dd9f6f042fe9c9be2da1822f0b38b5df
Jonathan Clem <jonathan@jclem.net>
2012-11-01 20:02:54 -0400

    New git functions
    • Moved git functions into git/functions
    • glg is now a function (instead of alias), can accept arguments

--- git/functions.zsh
+++
@@ -1,9 +0,0 @@
-current_branch () {
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo ${ref#refs/heads/}
-}
-
-git-local-remote () {
-  local branch=$1
-  git checkout -b $branch  --track origin/$branch
-}
