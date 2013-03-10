ab5df5b77d618e6542c7bc2d1844c7ac752e759c
Rich Soni <rich@ticketevolution.com>
2013-03-10 15:43:16 -0400

    Added sick todo script

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -14,6 +14,24 @@
   alias code='cd ~/Code'
   alias lvm='cd ~/Code/LVM/'
   alias mba='cd ~/Documents/MBA'
+  alias todo='cat -n ~/Documents/TodoLists/todo.txt'
+  todo.(){
+    echo "$1" >> ~/Documents/TodoLists/todo.txt;
+    todo;
+  } 
+  .todo(){
+    sed -i".bak" "$1d" ~/Documents/TodoLists/todo.txt;
+    todo;
+  }
+  alias wtodo='cat -n ~/Documents/TodoLists/work_todo.txt'
+  wtodo.(){
+    echo "$1" >> ~/Documents/TodoLists/work_todo.txt;
+    wtodo;
+  } 
+  .wtodo(){
+    sed -i".bak" "$1d" ~/Documents/TodoLists/work_todo.txt;
+    wtodo;
+  }
 #Ticket Evo Stuff
   alias tedocs='cd ~/Documents/TicketEvolution'
   alias be='bundle exec'
